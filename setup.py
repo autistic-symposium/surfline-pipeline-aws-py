343326f3ea9cb08ac64a36e7fc8e3e1cca8da736
bt3gl <steinkirch@protonmail.com>
2024-03-14 20:58:24 +0100

    quick fixes

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(
     name='rewind_clip_generator',
     version='1.0',
-    author="bt3gl",
+    author="Mia Stein",
     packages=['lib', 'lib.routes', 'lib.handlers'],
 )
